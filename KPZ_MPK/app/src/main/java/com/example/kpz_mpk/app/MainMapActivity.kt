be0186ce2b59e06c7b73959579ef2cad59909c35
MySzc <szczurekmaciej98@gmail.com>
2020-03-20 17:59:11 +0100

    Implemented map with a default location

---
+++ KPZ_MPK/app/src/main/java/com/example/kpz_mpk/app/MainMapActivity.kt
@@ -0,0 +1,45 @@
+package com.example.kpz_mpk.app
+
+import android.os.Bundle
+import com.example.kpz_mpk.R
+import com.example.kpz_mpk.app.core.BaseActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+
+
+class MainMapActivity : BaseActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Move the camera to a default location - Wroclaw's Main Square
+        val defaultLocation = LatLng(51.109897, 17.032752)
+        val defaultZoom= 14.0f
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, defaultZoom))
+        mMap.uiSettings.isRotateGesturesEnabled = false;
+    }
+}
